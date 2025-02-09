3aabdce139c2ee59a00bea83ed6a447b883ba1f6
Gonzalo Coradello <gonzalocoradello@gmail.com>
2025-02-09 08:12:49 -0300

    avoid showing null

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getAsync } = require('../redis');
 
 router.get('/', async (req, res) => {
-  const added_todos = await getAsync('added_todos');
+  const added_todos = await getAsync('added_todos') ?? '0';
 
   res.send({ added_todos });
 });
